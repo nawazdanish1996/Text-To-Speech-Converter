f89499a3349e46613b44b79a6f0e68e2dd66300e
Nawaz Danish <nawazdanish1996@gmail.com>
2022-07-05 03:48:28 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 const textarea = document.querySelector('textarea'),
+voiceList = document.querySelector('select');
 speechBtn = document.querySelector('button');
+
+let synth = speechSynthesis;
+
+function voices(){
+    for(let voice of synth.getVoices()){
+        console.log(voice);
+        // creating an option tag with passing voice anme and voice language.
+        let option = `<option value="${voice.name}">${voice.name} (${voice.name})</option>`;
+        voiceList.insertAdjacentHTML('beforeend', option); // iinserting option tag beforeend of select tag.
+    }
+}
+
+synth.addEventListener("voiceschanged", voices);
 
 function textToSpeech(text){
     let utternance = new SpeechSynthesisUtterance(text);
